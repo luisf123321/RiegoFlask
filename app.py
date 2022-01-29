2e3bb2fc0ea856f80c74abc3063bfdcc367689c4
blanca ligia peña escobar <blancaescobar@MacBookPro.local>
2022-01-29 16:29:56 -0500

    2

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ dblotes = client.Riego.lotes
 
 @app.route('/')
 def hello_world():
-    return 'Hello World RIEGO! Blanca'
+    return 'Hello World RIEGO! Blanca camio esto'
 
 
 @app.route('/users', methods = ['POST'])
