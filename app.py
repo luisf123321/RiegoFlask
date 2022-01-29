896dc0fd26a31d9e99ae8330da6018d3d02d119b
luis enciso <encisolf901@gmail.com>
2022-01-29 16:58:15 -0500

    dd

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ dblotes = client.Riego.lotes
 
 @app.route('/')
 def hello_world():
-    return 'Hello World RIEGO! enciso'
+    return 'Hello World RIEGO!'
 
 
 @app.route('/users', methods = ['POST'])
