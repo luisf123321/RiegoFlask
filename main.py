bc3e851b4a2ac1afbb36917524039caad06a8c10
luis fernando enciso <encisolf901@gmail.com>
2022-07-25 14:20:03 -0500

    cambios app

--- main.py
+++ main.py
@@ -14,10 +14,6 @@ from app import create_app
 
 
 app = create_app()
-#app.config['MONGO_URI'] = 'mongodb://localhost/riego2'
-#app.config['MONGO_URI'] = 'mongodb+srv://admin:admin@neivaroutes.ymunc.mongodb.net/Riego?retryWrites=true&w=majority'
-
-#mongo = PyMongo(app)
 
 CORS(app)
 
@@ -25,9 +21,6 @@ jwt = JWTManager(app)
 
 @app.route('/')
 def hello_world():
-    #usuario = Users.getUser(str(1022))
-    #usuario = Users.getUsers()
-    #print(type(jsonify(usuario.to_json(orient='columns'))))  
     return jsonify("hello world")
 
 if __name__ == '__main__':
